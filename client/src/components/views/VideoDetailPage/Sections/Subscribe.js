92451f0ac8195c350ba0de43b4812f87c227f503
Dapane-dev <daphned@naver.com>
2020-10-04 17:41:46 +0900

    구독 기능 추가

---
+++ client/src/components/views/VideoDetailPage/Sections/Subscribe.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react'
+import Axios from 'axios';
+
+
+function Subscribe(props) {
+    const userTo = props.userTo
+    const userFrom = props.userFrom
+
+    const [SubscribeNumber, setSubscribeNumber] = useState(0)
+    const [Subscribed, setSubscribed] = useState(false)
+
+    useEffect(() => {
+        let variable = { userTo: props.userTo }
+        Axios.post('/api/subscribe/subscribeNumber', variable)
+            .then(response => {
+                if (response.data.success) {
+                    setSubscribeNumber(response.data.subscribeNumber)
+                } else {
+                    alert('구독자 수 정보를 받아오지 못했습니다.')
+                }
+            })
+
+        Axios.post('/api/subscribe/subscribed', variable)
+            .then(response => {
+                if (response.data.success) {
+                    setSubscribed(response.data.subcribed)
+                } else {
+                    alert('구독자 정보를 받아오지 못했습니다.')
+                }
+            })
+
+    }, [])
+
+    const onSubscribe = () => {
+
+        let subscribeVariables = {
+                userTo : userTo,
+                userFrom : userFrom
+        }
+
+        if(Subscribed) {
+            Axios.post('/api/subscribe/unSubscribe', subscribeVariables)
+                .then(response => {
+                    if(response.data.success){ 
+                        setSubscribeNumber(SubscribeNumber - 1)
+                        setSubscribed(!Subscribed)
+                    } else {
+                        alert('구독 취소에 실패했습니다.')
+                    }
+                })
+
+        } else {
+            Axios.post('/api/subscribe/subscribe', subscribeVariables)
+                .then(response => {
+                    if(response.data.success) {
+                        setSubscribeNumber(SubscribeNumber + 1)
+                        setSubscribed(!Subscribed)
+                    } else {
+                        alert('구독 하기에 실패했습니다.')
+                    }
+                })
+        }
+
+    }
+
+    return (
+        <div>
+            <button 
+            onClick={onSubscribe}
+            style={{
+                backgroundColor: `${Subscribed ? '#AAAAAA' : '#CC0000'}`,
+                borderRadius: '4px', color: 'white',
+                padding: '10px 16px', fontWeight: '500', fontSize: '1rem', textTransform: 'uppercase'
+            }}>
+                {SubscribeNumber} {Subscribed ? '구독 중' : '구독'}
+            </button>
+        </div>
+    )
+}
+
+export default Subscribe
--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { List, Avatar, Row, Col } from 'antd';
 import SideVideo from './Sections/SideVideo';
+import Subscribe from './Sections/Subscribe';
 import Axios from 'axios';
 
 
@@ -32,7 +33,7 @@ function DetailVideoPage(props) {
                         <video style={{ width: '100%' }} src={`http://localhost:5000/${VideoDetail.filePath}`} controls></video>
 
                         <List.Item
-                            actions
+                            actions={[<Subscribe userTo={VideoDetail.writer._id}/>]}
                             
                         >
                             <List.Item.Meta
